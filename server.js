37717d7aff04ce46cf308c1db05cc51d9da97e32
Seth Bonnie <sethkbonnie@gmail.com>
2015-08-13 20:26:02 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var path = require( 'path' );
+var express = require( 'express' );
+var morgan = require( 'morgan' );
+var routes = require( './routes' );
+
+var port = 2336;
+var app = express();
+
+app.set( 'view engine', 'jade' );
+
+app.use( morgan('dev') );
+app.use( express.static(path.join(__dirname, 'public')) );
+app.use( routes );
+
+if ( !module.parent ) {
+  app.listen( port, function() {
+    console.log( 'Sitting at port ' + port + ', watching the tides roll away...' );
+  });
+}
+else {
+  module.exports = app;
+}
